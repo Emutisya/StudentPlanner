ea2bba8fcc3e56c75f3f70b98214efaf98248246
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-05-19 02:51:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/studentplanner/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.studentplanner;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+EditText etEmail, etPassword;
+TextView tvRegister;
+Button btnLogin;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEmail= (EditText) findViewById(R.id.et_email);
+        etPassword=(EditText) findViewById(R.id.et_password);
+        btnLogin=(Button) findViewById(R.id.btn_login);
+        tvRegister=(TextView) findViewById(R.id.tv_register);
+
+
+        tvRegister.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
