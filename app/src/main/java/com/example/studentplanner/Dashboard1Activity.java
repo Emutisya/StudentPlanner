1154e17f3b062f8d6e8c2cbe922f2fa2bddaca7a
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-05-19 08:34:39 +0300

    initial commit

--- app/src/main/java/com/example/studentplanner/Dashboard1Activity.java
+++ app/src/main/java/com/example/studentplanner/Dashboard1Activity.java
@@ -1,14 +1,39 @@
 package com.example.studentplanner;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Dashboard1Activity extends AppCompatActivity {
+    TextView tvSkip, tvNext;
+    ImageView backArrow;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard1);
+
+        backArrow=(ImageView) findViewById(R.id.iv_barrow);
+        tvSkip=(TextView) findViewById(R.id.tv_skip);
+        tvNext=(TextView) findViewById(R.id.tv_next);
+
+
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(Dashboard1Activity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/studentplanner/RegisterActivity.java
+++ app/src/main/java/com/example/studentplanner/RegisterActivity.java
@@ -1,14 +1,44 @@
 package com.example.studentplanner;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView tvLoginBack;
+    Button btnRegister;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        tvLoginBack =(TextView) findViewById(R.id.tv_login_back);
+        btnRegister=(Button) findViewById(R.id.btn_register);
+
+
+        tvLoginBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent( RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent( RegisterActivity.this, Dashboard1Activity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
