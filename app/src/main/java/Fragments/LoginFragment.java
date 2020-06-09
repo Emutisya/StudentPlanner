b960879994298e309bb33a51e3f2c6bec5caae2d
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-06-09 16:04:09 +0300

    initial commit

---
+++ app/src/main/java/Fragments/LoginFragment.java
@@ -0,0 +1,69 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.studentplanner.R;
+
+public class LoginFragment extends Fragment {
+
+    private View view;
+    EditText etEmail, etPassword;
+    TextView tvRegister;
+    Button btnLogin;
+
+    public LoginFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.login_layout, container, false);
+        init();
+
+        return view;
+    }
+
+    private void init() {
+        etEmail = view.findViewById(R.id.et_email);
+        etPassword = view.findViewById(R.id.et_password);
+        btnLogin = view.findViewById(R.id.btn_login);
+        tvRegister = view.findViewById(R.id.tv_register);
+
+        tvRegister.setOnClickListener(v -> {
+            //change fragments
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer, new RegisterFragment()).commit();
+
+        });
+        btnLogin.setOnClickListener(v -> {
+            //validate fields first
+            if (validate()) {
+                //do something
+            }
+
+        });
+    }
+
+    private boolean validate() {
+        if (etEmail.getText().toString().isEmpty()) {
+            etEmail.setError("Email is required");
+            return false;
+        }
+
+        if (etPassword.getText().toString().length() < 8) {
+            etPassword.setError("Password should be at least 8 characters");
+            return true;
+        }
+        return true;
+    }
+}
