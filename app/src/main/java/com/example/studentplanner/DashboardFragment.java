52721935b55bceafcd36ce583566f6640be869c2
EliStones <elisha.mbadi@strathmore.edu>
2020-06-24 12:27:49 +0300

    Dashboard View

--- app/src/main/java/com/example/studentplanner/DashboardFragment.java
+++ app/src/main/java/com/example/studentplanner/DashboardFragment.java
@@ -1,18 +1,30 @@
 package com.example.studentplanner;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardFragment extends Fragment {
+    private TextView textView;
+    private final String url_get_timetable = "";
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -23,6 +35,24 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_dashboard, container, false);
+        textView = root.findViewById(R.id.tv_dashboard);
+        loadFragmentData();
+        return root;
     }
+
+    private void loadFragmentData() {
+        ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonObjectRequest jsonObjRequest = new JsonObjectRequest(
+                Request.Method.GET, url_get_timetable, new Response.Listener<JSONObject>(){
+
+            @Override
+            public void onResponse(JSONObject response) {
+                progressDialog.dismiss();
+            }
+        });
+    };
 }
