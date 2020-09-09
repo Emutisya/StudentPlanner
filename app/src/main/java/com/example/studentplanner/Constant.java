6d6f7149c689c7d875d70e35a8cb1c25e524a961
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-09-10 00:37:40 +0300

    add journal entry(no photo)

--- app/src/main/java/com/example/studentplanner/Constant.java
+++ app/src/main/java/com/example/studentplanner/Constant.java
@@ -2,7 +2,7 @@ package com.example.studentplanner;
 //class to store URLs
 public class Constant {
 
-    public static final String URL ="http://f239a7b84bc4.ngrok.io/";
+    public static final String URL ="http://a88d9d40c74a.ngrok.io/";
     public static final String HOME =URL+"api";
     public static final String LOGIN =HOME+"/login";
     public static final String REGISTER =HOME+"/register";
